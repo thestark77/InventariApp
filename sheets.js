e383579488113db051fd9e574932c9c0a303ff77
Sebastian Florez <sebastianflorez3@gmail.com>
2020-05-31 13:35:02 -0500

    Proyect back to basic files, ready to start from NODE

--- sheets.js
+++ sheets.js
@@ -2,7 +2,7 @@
                                         CONSTANTS
 ======================================================================================================*/
 const { google } = require('googleapis');
-const keys = require('../keys.json');
+const keys = require('./keys.json');
 const client = new google.auth.JWT(keys.client_email, null, keys.private_key, ['https://www.googleapis.com/auth/spreadsheets']);
 const gsapi = google.sheets({ version: 'v4', auth: client });
 const RangeProducts = 'B3:C3759'; //Products table
